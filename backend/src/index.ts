a32f0e70e14a789f30a4d1d66e7b24ff11d134a8
Constantine Lobkov <lobkov@svyazcom.ru>
2020-08-09 22:14:49 +0800

    postgres init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,28 +1,25 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
+import { createConnection } from "typeorm";
+import { User } from "./entity/User";
 import * as Koa from "koa";
 import * as Router from "koa-router";
 import * as bodyParser from "koa-bodyparser";
-import {AppRoutes} from "./routes";
+import { AppRoutes } from "./routes";
 
-// create connection with database
-// note that its not active database connection
-// TypeORM creates you connection pull to uses connections from pull on your requests
-createConnection().then(async connection => {
-
-    // create koa app
+createConnection()
+  .then(async (connection) => {
     const app = new Koa();
     const router = new Router();
 
-    // register all application routes
-    AppRoutes.forEach(route => router[route.method](route.path, route.action));
+    AppRoutes.forEach((route) =>
+      router[route.method](route.path, route.action)
+    );
 
-    // run app
     app.use(bodyParser());
     app.use(router.routes());
     app.use(router.allowedMethods());
     app.listen(3000);
 
     console.log("Koa application is up and running on port 3000");
-
-}).catch(error => console.log("TypeORM connection error: ", error));
+  })
+  .catch((error) => console.log(error));
