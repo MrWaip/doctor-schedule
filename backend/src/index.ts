92081044e5479162b2ecbf9442a70f26bdc415cb
Constantine Lobkov <lobkov@svyazcom.ru>
2020-08-09 22:24:47 +0800

    линтер

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,22 @@
-import "reflect-metadata";
-import { createConnection } from "typeorm";
-import { User } from "./entity/User";
-import * as Koa from "koa";
-import * as Router from "koa-router";
-import * as bodyParser from "koa-bodyparser";
-import { AppRoutes } from "./routes";
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import * as Koa from 'koa';
+import * as Router from 'koa-router';
+import * as bodyParser from 'koa-bodyparser';
+import { AppRoutes } from './routes';
 
 createConnection()
-  .then(async (connection) => {
+  .then(async () => {
     const app = new Koa();
     const router = new Router();
 
-    AppRoutes.forEach((route) =>
-      router[route.method](route.path, route.action)
-    );
+    AppRoutes.forEach((route) => router[route.method](route.path, route.action));
 
     app.use(bodyParser());
     app.use(router.routes());
     app.use(router.allowedMethods());
     app.listen(3000);
 
-    console.log("Koa application is up and running on port 3000");
+    console.log('Koa application is up and running on port 3000');
   })
   .catch((error) => console.log(error));
