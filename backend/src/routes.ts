9db4217eb80b45a33259485aaa772c4a3bd274ab
Constantine Lobkov <lobkov@svyazcom.ru>
2020-08-10 23:13:05 +0800

    Маршруты

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,36 @@
 import { getDoctors } from './controller/DoctorController';
+import { getTimes, getDates } from './controller/TimeController';
+import { createSchedule, getSchedules, deleteById } from './controller/ScheduleController';
 
 export const ApiRoutes = [
   {
     path: '/doctors',
     method: 'get',
     action: getDoctors,
+  },
+  {
+    path: '/times',
+    method: 'get',
+    action: getTimes,
+  },
+  {
+    path: '/schedule',
+    method: 'post',
+    action: createSchedule,
+  },
+  {
+    path: '/schedule',
+    method: 'get',
+    action: getSchedules,
+  },
+  {
+    path: '/dates',
+    method: 'get',
+    action: getDates,
+  },
+  {
+    path: '/schedule/:id',
+    method: 'delete',
+    action: deleteById,
   },
 ];
